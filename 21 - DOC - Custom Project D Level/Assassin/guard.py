21b6f518fd871c95e85629f2fe436c1498128da0
mattcoulter7 <mattcoul7@gmail.com>
2020-06-03 12:25:45 +1000

    Moved vision into it's own class

--- 21 - DOC - Custom Project D Level/Assassin/guard.py
+++ 21 - DOC - Custom Project D Level/Assassin/guard.py
@@ -8,6 +8,7 @@ from path import Path
 from search_functions import astar,smooth
 from weapon import Weapon
 from history import History
+from vision import Vision
 
 class Guard(object):
 
@@ -46,8 +47,7 @@ class Guard(object):
         self.hearing_range = 5.0
 
         # AI Vision
-        self.vision_range = 5
-        self.vision = []
+        self.vision = Vision(self,5,self.world)
         self.history = History(self,self.world)
 
         # AI Wandering
@@ -60,7 +60,7 @@ class Guard(object):
         ''' update vehicle position and orientation '''
         self.history.update()
         # Refresh what can be seen by guard
-        self.update_vision()
+        self.vision.update()
         # update mode if necessary
         self.update_mode()
 
@@ -87,34 +87,7 @@ class Guard(object):
         else:
             self.mode = 'scout'
 
-    def update_vision(self):
-        # Adjacent edges of node
-        adjacent = [(1,0),(0,1),(-1,0),(0,-1),(1,1),(1,-1),(-1,1),(-1,-1)]
-        # Nodes that the guard will be able to see
-        visible_nodes = []
-        # Distance to middle point of vision square that will be made
-        ahead_range = int((self.vision_range - 1) / 2)
-        # Node of self
-        self_node = self.world.graph.pos_to_node(self.pos.copy())
-        # Middle point of vision square that will be made
-        ahead_node = self.world.graph.pos_to_node(self.pos + self.heading * self.world.graph.grid_size * ahead_range)
-        # Add that centre point to begin with
-        visible_nodes.append(ahead_node)
-        # Expand from centre outwards adding nodes to cover full square
-        check_from = 0 # Used to avoid retesting adjacent nodes that we have already checked for
-        for i in range(ahead_range):
-            for j in range(check_from,len(visible_nodes)):
-                for pt2 in adjacent:
-                    pt1 = visible_nodes[j]
-                    new_node = Vector2D(pt1.x + pt2[0],pt1.y + pt2[1])
-                    if self.world.graph.node_available(new_node) and new_node not in visible_nodes:
-                        visible_nodes.append(new_node)
-                        check_from += 1
-        for i in range(len(visible_nodes)):
-            visible_nodes[i] = self.world.graph.global_to_relative(visible_nodes[i])
-            self.history.remove(visible_nodes[i]) # Remove point from yet_to_visit list
 
-        self.vision = visible_nodes
 
     def render(self, color=None):
         ''' Draw the Guard'''
@@ -131,10 +104,7 @@ class Guard(object):
         else: # Still
             self.still.update(x=x_val,y=y_val,rotation=angle)
             self.still.draw()
-        # Visibility
-        for pt in self.vision:
-            egi.red_pen()
-            egi.circle(self.world.graph.node_to_pos(pt.copy(),'center'),self.world.graph.grid_size/2)
+        self.vision.render()
         # Weapon and Bullets
         self.weapon.render()
 
@@ -153,7 +123,7 @@ class Guard(object):
     def see_assassin(self):
         ''' Returns true if the assassin is within the vision blocks '''
         assassin_node = self.world.graph.pos_to_node(self.world.assassin.pos.copy())
-        return assassin_node in self.vision
+        return assassin_node in self.vision.vision
 
     def intersect_pos(self,pos):
         ''' Returns true if assassin intersects a particular position'''
---
+++ 21 - DOC - Custom Project D Level/Assassin/vision.py
@@ -0,0 +1,45 @@
+from vector2d import Vector2D
+from graphics import egi
+
+class Vision(object):
+    """description of class"""
+    def __init__(self,parent,range,world):
+        self.parent = parent
+        self.vision_range = range
+        self.world = world
+        self.vision = []
+
+    def update(self):
+        # Adjacent edges of node
+        adjacent = [(1,0),(0,1),(-1,0),(0,-1),(1,1),(1,-1),(-1,1),(-1,-1)]
+        # Nodes that the guard will be able to see
+        visible_nodes = []
+        # Distance to middle point of vision square that will be made
+        ahead_range = int((self.vision_range - 1) / 2)
+        # Node of self
+        self_node = self.world.graph.pos_to_node(self.parent.pos.copy())
+        # Middle point of vision square that will be made
+        ahead_node = self.world.graph.pos_to_node(self.parent.pos + self.parent.heading * self.world.graph.grid_size * ahead_range)
+        # Add that centre point to begin with
+        visible_nodes.append(ahead_node)
+        # Expand from centre outwards adding nodes to cover full square
+        check_from = 0 # Used to avoid retesting adjacent nodes that we have already checked for
+        for i in range(ahead_range):
+            for j in range(check_from,len(visible_nodes)):
+                for pt2 in adjacent:
+                    pt1 = visible_nodes[j]
+                    new_node = Vector2D(pt1.x + pt2[0],pt1.y + pt2[1])
+                    if self.world.graph.node_available(new_node) and new_node not in visible_nodes:
+                        visible_nodes.append(new_node)
+                        check_from += 1
+        for i in range(len(visible_nodes)):
+            visible_nodes[i] = self.world.graph.global_to_relative(visible_nodes[i])
+            self.parent.history.remove(visible_nodes[i]) # Remove point from yet_to_visit list
+
+        self.vision = visible_nodes
+
+    def render(self):
+        # Visibility
+        for pt in self.vision:
+            egi.red_pen()
+            egi.circle(self.world.graph.node_to_pos(pt.copy(),'center'),self.world.graph.grid_size/2)
